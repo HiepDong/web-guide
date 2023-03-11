702c540223ae212ef7fe34ef108f8d34e68eb21c
HiepDong <119558835+HiepDong@users.noreply.github.com>
2023-03-11 18:29:23 +0700

    Add files via upload

---
+++ app/js/scripts.js
@@ -0,0 +1,26 @@
+/*!
+ * Start Bootstrap - Modern Business v5.0.5 (https://startbootstrap.com/template-overviews/modern-business)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+ */
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+function get_Text() {
+  return document.getElementById(`test`);
+}
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log("Name: " + profile.getName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log("User signed out.");
+  });
+}
